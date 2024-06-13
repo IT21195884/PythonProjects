f9cc5f8f5df7ddb344653509dbfec6dc9fc237c8
IT21195884 <nelithhansindu@gmail.com>
2024-06-13 23:20:53 +0530

    madlibs project commit - 01(Initial)

---
+++ madlibs_Proj01/madlibs.py
@@ -0,0 +1,20 @@
+#String Concatenation
+#Suppose we need to create a string that says "My Name is ______ !"
+
+# name="Hansindu Abeykoon"
+
+# print("My name is "+name)
+# print("My name is {}".format(name))
+# print(f"My name is {name}")
+#X
+
+adj = input("Adjective : ")
+verb1= input("Verb : ")
+verb2= input("Verb : ")
+famousPerson= input("Famous person : ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited  all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famousPerson}!"
+
+print(madlib)
+ 
